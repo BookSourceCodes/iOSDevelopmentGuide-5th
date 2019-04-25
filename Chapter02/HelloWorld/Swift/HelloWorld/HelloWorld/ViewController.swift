571b3da49af82fb80824720b6ae531de67a7ab77
xiaotuan <qin_ty@yuntianzn.com>
2019-04-25 17:54:29 +0800

    添加第2章=>HelloWorld->HelloWorld示例程序

---
+++ Chapter02/HelloWorld/Swift/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by QinTuanye on 2019/4/25.
+//  Copyright © 2019年 QinTuanye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.txt
@@ -0,0 +1,6 @@
+iOSDevelopmentGuide-5th
+
+iOS 开发指南 （第5版） 原书配套源代码
+
+Chapter02
+    01. HelloWorld                                      ===> HelloWorld示例程序
